9e1e7af393de6fb910d1b56ef6ead7408d884f67
Hawkins <fincke@gmx.de>
2017-05-30 23:39:31 +0200

    log output

--- src/main/java/de/sfincke/myrestapp/controller/KundeController.java
+++ src/main/java/de/sfincke/myrestapp/controller/KundeController.java
@@ -1,6 +1,7 @@
 package de.sfincke.myrestapp.controller;
 
 import de.sfincke.myrestapp.model.Customer;
+import lombok.extern.java.Log;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import java.time.LocalDate;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@Log
 public class KundeController {
 
     private static final String template = "Hello, %s!";
@@ -17,8 +19,9 @@ public class KundeController {
     @RequestMapping("/customer")
     public Customer greeting(@RequestParam(value = "name", defaultValue = "Max") String name,
                              @RequestParam(value = "lastName", defaultValue = "Mustermann") String lastName) {
+        log.info("Requesting a new customer!!");
         return new Customer(counter.getAndIncrement(), name, lastName,
-                LocalDate.of(1977, 03, 29));
+                LocalDate.of(1977, 3, 29));
     }
 
 }
